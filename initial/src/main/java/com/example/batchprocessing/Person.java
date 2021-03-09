8f448e7da080969b6e8f667baff18b8246f6d627
Nao Minami <south37777@gmail.com>
2021-03-09 22:45:33 +0900

    Add batch

---
+++ initial/src/main/java/com/example/batchprocessing/Person.java
@@ -0,0 +1,38 @@
+package com.example.batchprocessing;
+
+public class Person {
+
+    private String lastName;
+    private String firstName;
+
+    public Person() {
+    }
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "firstName: " + firstName + ", lastName: " + lastName;
+    }
+
+}
